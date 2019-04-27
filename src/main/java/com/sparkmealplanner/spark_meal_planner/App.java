c6ea4447fc788018b617f0ad77991802505d56a7
vineelapentyala <pentyala@seas.upenn.edu>
2019-04-27 09:18:38 -0400

    updated calendarHandler and App classes

--- src/main/java/com/sparkmealplanner/spark_meal_planner/App.java
+++ src/main/java/com/sparkmealplanner/spark_meal_planner/App.java
@@ -1,5 +1,9 @@
 package com.sparkmealplanner.spark_meal_planner;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+
 /**
  * This class contains the main method and can be program can be run from this
  * class
@@ -7,7 +11,12 @@ package com.sparkmealplanner.spark_meal_planner;
  */
 public class App {
 	public static void main(String[] args) {
-		MealPlanner mp = new MealPlanner();
-		mp.run();
+//		MealPlanner mp = new MealPlanner();
+//		mp.run();
+		CalendarHandler calendarHandler = new CalendarHandler();
+		HomePageHandler homePageHandler = new HomePageHandler();
+		port(8080);
+		get("/",homePageHandler);
+		get("/Calendar", calendarHandler);
 	}
 }
--- src/main/java/com/sparkmealplanner/spark_meal_planner/CalendarHandler.java
+++ src/main/java/com/sparkmealplanner/spark_meal_planner/CalendarHandler.java
@@ -6,9 +6,85 @@ import spark.Route;
 
 public class CalendarHandler implements Route {
 
-	private final String htmlHead = "<html><head><title>Meal Planner Calendar</title></head>";
-	public Object handle(Request request, Response response) throws Exception {
-		return htmlHead + "<body><h3>Meal Planner Calendar</h3>"+
-	"<button><a href=\"/todo\">Go back to your list</a></button></body></html>";
-	}
+    private final String htmlHead = "<html><head><title>Meal Planner Calendar</title></head>";
+
+    public Object handle(Request request, Response response) throws Exception {
+	return htmlHead + "<body><h3>Meal Planner Calendar</h3>" + 
+		"<div class=\"dropdown\">\r\n" + 
+		"<div class=\"dropdown-content\">\r\n" + 
+		"  <select>\r\n" + 
+		"  <option value=\"Sunday\">Sunday</option>\r\n" + 
+		"  <option value=\"Monday\">Monday</option>\r\n" + 
+		"  <option value=\"Tuesday\">Tuesday</option>\r\n" + 
+		"  <option value=\"Wednesday\">Wednesday</option>\r\n" +
+		"  <option value=\"Thursday\">Thursday</option>\r\n" + 
+		"  <option value=\"Friday\">Friday</option>\r\n" + 
+		"  <option value=\"Saturday\">Saturday</option>\r\n" + 
+		"  </select>" +
+		"  </div>\r\n" + 
+		"</div>" +
+		"<div class=\"dropdown-content\">\r\n" + 
+		"  <select>\r\n" + 
+		"  <option value=\"Breakfast\">Breakfast</option>\r\n" + 
+		"  <option value=\"Lunch\">Lunch</option>\r\n" + 
+		"  <option value=\"Snack\">Snack</option>\r\n" + 
+		"  <option value=\"Dinner\">Dinner</option>\r\n" +
+		"  </select>" +
+		"  </div>\r\n" + 
+		"</div>" +
+		"<button>Submit</button>\r\n"+
+		"<table>\r\n" + 
+		"  <tr>\r\n" + 
+		"    <th>Meal</th>\r\n" + 
+		"    <th>Sunday</th>\r\n" + 
+		"    <th>Monday</th> \r\n" + 
+		"    <th>Tuesday</th>\r\n" + 
+		"    <th>Wednesday</th>\r\n" + 
+		"    <th>Thursday</th>\r\n" + 
+		"    <th>Friday</th>\r\n" + 
+		"    <th>Saturday</th>\r\n" + 
+		"  </tr>\r\n" + 
+		"  <tr>\r\n" + 
+		"  	<th>Breakfast</th>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td> \r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  </tr>\r\n" + 
+		"  <tr>\r\n" + 
+		"  	<th>Lunch</th>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td> \r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  </tr>\r\n" + 
+		"  <tr>\r\n" + 
+		"  	<th>Snack</th>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td> \r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  </tr>\r\n" + 
+		"  <tr>\r\n" + 
+		"  	<th>Dinner</th>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td> \r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  	<td>Not picked yet</td>\r\n" + 
+		"  </tr>\r\n" + 
+		"</table>\r\n" + 
+		"<button><a href=\"/todo\">Go back to your list</a></button></body></html>";
+    }
 }
