2fc7492ca9ffcb2ba2cad1e2ff6826e4e03455e6
vineelapentyala <pentyala@seas.upenn.edu>
2019-04-30 19:55:58 -0400

    It is not complete yet

--- src/main/java/com/sparkmealplanner/spark_meal_planner/ManualRecipeInputHandler.java
+++ src/main/java/com/sparkmealplanner/spark_meal_planner/ManualRecipeInputHandler.java
@@ -19,7 +19,7 @@ public class ManualRecipeInputHandler implements Route{
 	private ArrayList<String> manCookingSteps;
 	private double manCookingTimeInSeconds;
 	private int manNumOfPeopleToServe;
-	public final String IngredientForm = "<div><form action =\"/createIngredientList\" method = \"post\"> Add new Ingredient:"+
+	public final String IngredientForm = "<div><form action =\"/addIngredient\" method = \"post\"> Add new Ingredient:"+
 	"<input type = \"text\" name=\"ingredientLine\">" +
 	"<button style = \"margin-left: 10px\" type=\"submit\">Add Ingredient</button>"+
 	"</form><div>";
@@ -31,68 +31,47 @@ public class ManualRecipeInputHandler implements Route{
 	    manDishName ="";
 	    manIngredients = new ArrayList<Ingredient>();
 	}
-	public String RecipeName() {
+	public String RecipeNameForm() {
 	    StringBuilder sb = new StringBuilder();
 	    sb.append("<div> <form>\r\n" + 
 	    	"  Recipe Name:<br>\r\n" + 
-	    	"  <input type=\"text\" name=\"recipename\">\r\n" + 
+	    	"  <input type=\"text\" name=\"recipename\"required>\r\n" + 
 	    	"  <br></form></div>");
 	    sb.append("<button><a href=\"/recipeName\">Submit</a></button>");
 	    return sb.toString();
 	}
+	
+	public String RecipeURLForm() {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("<div> <form>\r\n" + 
+	    	"  Recipe URL:<br>\r\n" + 
+	    	"  <input type=\"URL\" name=\"recipeURL\"required>\r\n" + 
+	    	"  <br></form></div>");
+	    return sb.toString();
+	}
+	
+	public String IngredientListForm() {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("<div><form>\r\n" + 
+	    	"    <p>\r\n" + 
+	    	"        <label>Ingredient Name:</label> <input type=\"text\">\r\n" + 
+	    	"        <span class=\"remove\">Remove</span>\r\n" + 
+	    	"    </p>\r\n" + 
+	    	"    <p>\r\n" + 
+	    	"        <span class=\"add\">Add fields</span>\r\n" + 
+	    	"    </p>\r\n" + 
+	    	"</form><div>");
+	    return sb.toString();
+	}
+	
 	public String IngredientList() {
 	    StringBuilder sb = new StringBuilder();
 	    sb.append("<div><ol>");
-	    int ingredientCounter =0;
 	    for (Ingredient ingredient : manIngredients) {
-		ingredientCounter++;
-		sb.append("<li>" + ingredient.getIngredientLine() + "<button style = \"margin-left: 10px\" onclick = " +
-			"\"location.href/Ingredients?id=" +  ingredientCounter + "'\">Add More</button></li>");
+		sb.append("<li>" + ingredient.getIngredientLine() + "</li>");
 	    }
 	    sb.append("</ol></div>");
 	    return sb.toString();
-	}
-	/*
-	 * The manualDishBuilding method is used to get input from the user through the
-	 * We will create a dish object from this in the main method when the user
-	 * decides to manually input a recipe instead of searching from an API
-	 */
-	public void manualDishBuilding() {
-		Scanner manualInput = new Scanner(System.in);
-
-		// asking for the name of the dish
-		System.out.println("What is the name of the Dish?");
-		manDishName = manualInput.nextLine();
-		// asking for the ingredients
-		System.out.println("Please enter a List of indgredients and press Done");
-		// The while loop below stops here when the user enters "Done" and for now it is
-		// assumed that the user will correctly
-		// enter done when done with inputting the ingredients. This will be connected
-		// with clicking a done button on HTML
-		while (!manualInput.nextLine().equals("Done")) {
-			String s = manualInput.nextLine();
-			Ingredient i = new Ingredient(s);
-			manIngredients.add(i);
-		}
-		// Similar to the above step, the while loop below for manually entering steps
-		// for cooking stops when the user enters "Done" and
-		// for now it is assumed that the user will correctly enter done when done with
-		// inputting the ingredients
-		System.out.println("Please enter a List of steps to be followed for cooking and press Done");
-		while (!manualInput.nextLine().equals("Done")) {
-			String cookingStep = manualInput.nextLine();
-			manCookingSteps.add(cookingStep);
-		}
-
-		// asking for the cooking time
-		System.out.println("What is the cooking time in seconds?");
-		manCookingTimeInSeconds = manualInput.nextDouble();// The user is expected to enter cooking time in seconds.
-
-		// serving size
-		System.out.println("Hom many people does this recipe serve?");
-		manNumOfPeopleToServe = manualInput.nextInt();
-
-		manualInput.close();
 	}
 
 	/**
@@ -144,13 +123,13 @@ public class ManualRecipeInputHandler implements Route{
 	    if ("/recipeName".equals(request.pathInfo())) {
 		manDishName = request.queryParams("recipename");
 	    }
-	    else if ("/createIngredientList".contentEquals(request.pathInfo())){
+	    else if ("/addIngredient".contentEquals(request.pathInfo())){
 		String ingredientLine = request.queryParams("ingredientLine");
 		Ingredient i = new Ingredient(ingredientLine);
 		manIngredients.add(i);
 	    }
 	    return TagCreator.gethtmlHead("Meal Planner Calendar") + 
-		    RecipeName() + IngredientList() + IngredientForm +
+		    RecipeNameForm() + RecipeURLForm() + IngredientListForm() + IngredientList() + 
 		    TagCreator.getFooter() + "</body></html>";
 	}
 
