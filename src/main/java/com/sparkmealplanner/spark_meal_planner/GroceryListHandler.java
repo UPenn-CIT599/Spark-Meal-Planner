8cac4921ee0171ecdcabf20410d0a8ca787cd60f
unknown <xliu28@seas.upenn.edu>
2019-05-02 00:59:44 -0500

    Edited display method

--- src/main/java/com/sparkmealplanner/spark_meal_planner/GroceryListHandler.java
+++ src/main/java/com/sparkmealplanner/spark_meal_planner/GroceryListHandler.java
@@ -9,56 +9,76 @@ import spark.Response;
 import spark.Route;
 
 public class GroceryListHandler implements Route {
-	
+
 	private ArrayList<Dish> listOfDish;
 	private HashMap<String, ArrayList<Ingredient>> groceryList;
 	private String dishName;
 	private ArrayList<Ingredient> listOfIngredients;
-	
+
+	/**
+	 * The constructor initiate the dish list from the calendar
+	 */
 	public GroceryListHandler() {
-		HashMap <String, Dish> calendarHashMap = CalendarHandler.getCalendarHashMap();
-		//loop through the HashMap and create an arrayliList of Dish objects.
-		
+		HashMap<String, Dish> calendarHashMap = CalendarHandler.getCalendarHashMap();
+		listOfDish = new ArrayList<Dish>();
+		// loop through the HashMap and create an arrayliList of Dish objects.
+		for (Entry<String, Dish> mealsCalendar : calendarHashMap.entrySet()) {
+			Dish dishOnCalendar = mealsCalendar.getValue();
+			if (dishOnCalendar != null) {
+				listOfDish.add(dishOnCalendar);
+			}
+		}
 	}
-	
+
 	public Object handle(Request request, Response response) throws Exception {
-		return TagCreator.gethtmlHead("Grocery List") 
-				+ "<body><h3>Grocery List</h3>"
-				+ displayGroceryList()
-				+ "<button><a href=\"/\">Go back to your list</a></button></body></html>"
-				+TagCreator.getFooter();
+
+		return TagCreator.gethtmlHead("Grocery List") + TagCreator.createBodyTitle("Grocery List")
+				+ displayGroceryList() + TagCreator.createPrintThisButton() + TagCreator.getFooter()
+				+ TagCreator.closeTag();
 	}
-	
+
+	/**
+	 * The method is to display grocery list after item is selected to calendar
+	 * 
+	 * @return groceryList
+	 */
 	public String displayGroceryList() {
-		
-		GroceryList grocery = new GroceryList(listOfDish);
+
+		GroceryListHandler glh = new GroceryListHandler();
+		GroceryList grocery = new GroceryList(glh.listOfDish);
 		groceryList = grocery.getGroceryListFromListOfDishes();
-		
-		
-//		//call the list of dish names
-//		Calendar calendar = new Calendar();
-//		ArrayList<Dish> listOfDish = new ArrayList<Dish>();
-//		ArrayList<String> listOfDishNames = new ArrayList<String>();
-////		listOfDishNames = calendar.getCalendar().get(key);
-		
+
 		StringBuilder sb = new StringBuilder();
-		
-		sb.append("<div><ul>");
-		
-		for(Entry<String, ArrayList<Ingredient>> groceryItems: groceryList.entrySet()) {
+
+		sb.append("<div><ul style=\\\"list-style-type:disc;\\\">");
+
+		for (Entry<String, ArrayList<Ingredient>> groceryItems : groceryList.entrySet()) {
+			
 			dishName = groceryItems.getKey();
 			listOfIngredients = groceryItems.getValue();
-			sb.append("<ul><li>" + "<p><b>"+"Dish Name: "+dishName+"</b><p>" + "</li></ul>");
-				for(Ingredient eachIngredient:listOfIngredients) {
-					sb.append("<ol><li>" + eachIngredient 
-							+ "<button style=\"margin-left: 10px\" onclick=\""
-							+ listOfIngredients.remove(eachIngredient)
-							+ "'\">Remove from shopping list</button>" 	
-							+ "</li></ol>");
-				}
+			
+			sb.append("<li>" + "<p><b>" + "Dish Name: " + dishName + "</b><p>" + "</li>");
+			
+			for (Ingredient eachIngredient : listOfIngredients) {
+
+				sb.append("<li>" + eachIngredient.getIngredientLine() 
+						+"<button style=\"margin-left: 10px\" type=\"submit\">Remove</button>"
+						+ "</li>"
+						);
+
+				// sb.append("<ol><li>" + eachIngredient
+				// + "<button style=\"margin-left: 10px\" onclick=\""
+				// + listOfIngredients.remove(eachIngredient)
+				// + "'\">Remove from shopping list</button>"
+				// + "</li></ol>");
+			}
+
+			// sb.append("<div></ul>");
 		}
-		
+
+		sb.append("<div><br></ul>");
+
 		return sb.toString();
-		
+
 	}
 }
